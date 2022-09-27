4cf942d5dc00ddaf1966d69eaf03cd2ca1c316a6
SmaLLAlien <vlad_belousov1990@hotmail.com>
2022-09-27 19:49:02 +0300

    layoutCDK: added breakpoint observer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ComponentPortal} from "@angular/cdk/portal";
 import {Platform} from "@angular/cdk/platform";
 import {Overlay, OverlayPositionBuilder} from "@angular/cdk/overlay";
-import {BreakpointObserver} from "@angular/cdk/layout";
+import {BreakpointObserver, Breakpoints} from "@angular/cdk/layout";
 import {DialogComponent} from "./overlay-example/dialog/dialog.component";
+import {Observable} from "rxjs";
+import {map} from "rxjs/operators";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-features';
+  isWideScreen: Observable<boolean>
 
   constructor(
     private overly: Overlay,
     private positionBuilder: OverlayPositionBuilder,
+    public platform: Platform,
+    private breakpointObserver: BreakpointObserver
   ) {}
+
+  ngOnInit(): void {
+      if (this.breakpointObserver.isMatched('(max-width: 600px)')) {
+        console.info(1)
+      }
+
+      this.isWideScreen = this.breakpointObserver.observe([Breakpoints.HandsetLandscape])
+        .pipe(map(({matches}) => matches))
+    }
 
   createDialog() {
     const overlayRef = this.overly.create({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,23 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { OverlayExampleModule } from "./overlay-example/overlay-example.module";
 import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatButtonModule} from "@angular/material/button";
+import {LayoutModule} from "@angular/cdk/layout";
 
 
 @NgModule({
   declarations: [
     AppComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    OverlayExampleModule,
-    MatToolbarModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        OverlayExampleModule,
+        MatToolbarModule,
+        MatButtonModule,
+        LayoutModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
