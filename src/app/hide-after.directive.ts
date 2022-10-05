607810cc33a6b5db458da9db08bb95785a14b84d
SmaLLAlien <vlad_belousov1990@hotmail.com>
2022-10-05 16:54:04 +0300

    structural-directive: added context

--- src/app/hide-after.directive.ts
+++ src/app/hide-after.directive.ts
@@ -1,27 +1,48 @@
 import {Directive, Input, OnInit, TemplateRef, ViewContainerRef} from '@angular/core';
 
+class HideAfterContext {
+  public get $implicit() {
+    return this.hideAfter;
+  }; // $implicit - default value
+  public hideAfter = 0;
+  public counter = 0;
+}
+
 @Directive({
   selector: '[hideAfter]'
 })
 export class HideAfterDirective implements OnInit {
-  @Input('hideAfter') delay: number = 0;
+  @Input('hideAfter')
+  set delay(value: number | null) {
+    this._delay = value ?? 0;
+    this.context.hideAfter = this._delay / 1000;
+    this.context.counter = this._delay / 1000;
+  }
+  private _delay: number = 0;
 
   @Input('hideAfterLater') placeholder: TemplateRef<any> | null = null;
+
+  private context = new HideAfterContext();
 
   constructor(private viewContainerRef: ViewContainerRef,
               private template: TemplateRef<any>) { }
 
   ngOnInit() {
-    this.viewContainerRef.createEmbeddedView(this.template);
+    this.viewContainerRef.createEmbeddedView(this.template, this.context);
+
+    const intervalId = setInterval(
+      () => this.context.counter--,
+      1000)
 
     setTimeout(
       () => {
         this.viewContainerRef.clear();
         if (this.placeholder) {
-          this.viewContainerRef.createEmbeddedView(this.placeholder);
+          this.viewContainerRef.createEmbeddedView(this.placeholder, this.context);
         }
+        clearInterval(intervalId);
       },
-      this.delay);
+      this._delay);
   }
 
 }
