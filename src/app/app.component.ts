88a4cb7a6cf8c915f5cd0a8c881fbea7b6982495
SmaLLAlien <vlad_belousov1990@hotmail.com>
2022-10-10 16:36:43 +0300

    menu: added menu example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-features';
+
+  toggleMenu(menuToggle: HTMLDivElement) {
+    menuToggle.classList.toggle('active');
+  }
 }
