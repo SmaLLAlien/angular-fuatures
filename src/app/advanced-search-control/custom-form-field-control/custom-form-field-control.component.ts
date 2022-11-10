6a3acacc5002c8e807b2bf5acb9d1086fb11dda2
SmaLLAlien <vlad_belousov1990@hotmail.com>
2022-11-10 23:32:03 +0200

    custom-form-field: part2

--- src/app/advanced-search-control/custom-form-field-control/custom-form-field-control.component.ts
+++ src/app/advanced-search-control/custom-form-field-control/custom-form-field-control.component.ts
@@ -1,15 +1,29 @@
 import {
-  Component, ElementRef, HostBinding, Input, OnDestroy, OnInit, ViewChild,
+  Component, ElementRef, HostBinding, Input, OnDestroy, OnInit, Optional, Self, ViewChild,
 } from '@angular/core';
 import { MatFormFieldControl } from '@angular/material/form-field';
-import {AbstractControlDirective, NgControl} from "@angular/forms";
-import {Subject} from "rxjs";
+import {
+  ControlValueAccessor,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  NgControl
+} from "@angular/forms";
+import {Subject, take} from "rxjs";
 import {FocusMonitor} from "@angular/cdk/a11y";
 import {MatInput} from "@angular/material/input";
+import {ErrorStateMatcher} from "@angular/material/core";
 
 export interface FormFieldValue {
   query: string;
   scope: string;
+}
+
+export class CustomErrorMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl): boolean {
+    return control.dirty && control.invalid;
+  }
+
 }
 
 @Component({
@@ -20,21 +34,24 @@ export interface FormFieldValue {
     {
       provide: MatFormFieldControl,
       useExisting: CustomFormFieldControlComponent,
+    },
+    {
+      provide: ErrorStateMatcher,
+      useClass: CustomErrorMatcher
     }
   ],
 })
-export class CustomFormFieldControlComponent implements MatFormFieldControl<FormFieldValue>, OnInit, OnDestroy {
+export class CustomFormFieldControlComponent implements MatFormFieldControl<FormFieldValue>, OnInit, OnDestroy, ControlValueAccessor {
   @ViewChild(MatInput, {read: ElementRef, static: true}) input: ElementRef;
   static nextId = 0;
   @Input()
   set value(value: FormFieldValue) {
-    this._value = value;
+    this.form.patchValue(value);
     this.stateChanges.next();
   }
   get value() {
-    return this._value;
+    return this.form.value;
   }
-  private _value: FormFieldValue;
 
   @Input()
   set placeholder(placeholder: string) {
@@ -56,9 +73,11 @@ export class CustomFormFieldControlComponent implements MatFormFieldControl<Form
   get empty() {
     return !this.value.query && !this.value.scope;
   }
-  errorState = false;
+
+  get errorState() {
+    return this.errorMatcher.isErrorState(this.ngControl.control, null);
+  };
   focused: boolean;
-  readonly ngControl: NgControl | AbstractControlDirective | null = null;
 
   onContainerClick(event: MouseEvent): void {
     this.focusMonitor.focusVia(this.input, 'program');
@@ -78,7 +97,20 @@ export class CustomFormFieldControlComponent implements MatFormFieldControl<Form
 
   readonly userAriaDescribedBy: string;
 
-  constructor(private focusMonitor: FocusMonitor) {}
+  form: FormGroup;
+
+  constructor(private focusMonitor: FocusMonitor,
+              private fb: FormBuilder,
+              private errorMatcher: ErrorStateMatcher,
+              @Optional() @Self() public ngControl: NgControl) {
+    if (this.ngControl) {
+      this.ngControl.valueAccessor = this;
+    }
+    this.form = this.fb.group({
+      scope: new FormControl(''),
+      query: new FormControl(''),
+    })
+  }
 
   ngOnDestroy(): void {
         this.focusMonitor.stopMonitoring(this.input);
@@ -92,6 +124,34 @@ export class CustomFormFieldControlComponent implements MatFormFieldControl<Form
         this.focused = !!focused;
         this.stateChanges.next();
       });
+    this.focusMonitor.monitor(this.input).pipe(take(1)).subscribe(() => {
+      this.onTouch();
+    });
+
+    this.form.valueChanges.subscribe(value => this.onChange(value));
+  }
+
+  readonly autofilled: boolean;
+
+  onChange: (value: FormFieldValue) => void
+  onTouch: () => void
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouch = fn;
+  }
+
+  writeValue(obj: FormFieldValue): void {
+    this.form.patchValue(obj);
+  }
+
+  setDisabledState(isDisabled: boolean) {
+    this.disabled = isDisabled;
+    this.form.disable();
+    this.stateChanges.next();
   }
 
 }
